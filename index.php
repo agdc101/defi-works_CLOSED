ebac1e060578a8100c4f0981d7916296eb7aada4
agdc101 <ashleycampbell@MacBook-Pro.local>
2023-02-16 19:42:25 +0000

    form fields added

--- index.php
+++ index.php
@@ -26,14 +26,17 @@
   }
   $conn->close();
 ?>
-
-<form action="formSubmitHandler.php" method="post">
-    First name: <input type="text" name="fname"><br>
-    Last name: <input type="text" name="lname"><br>
-    Email: <input type="email" name="email"><br>
-    Password: <input type="text" name="userPassword">
-    <input type="submit">
-</form>
+<div class="register-form-wrapper">
+  <form id="register-form" action="formSubmitHandler.php" method="post">
+      First name: <input type="text" name="fname"><br>
+      Last name: <input type="text" name="lname"><br>
+      Email: <input type="email" name="email"><br>
+      Confirm Email: <input type="email" name="email"><br>
+      Password: <input type="text" name="userPassword"><br>
+      Confirm Password: <input type="text" name="userPassword"><br>
+      <input type="submit">
+  </form>
+</div>
 
 
 
